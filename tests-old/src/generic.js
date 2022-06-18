11d3ed500a86116514c625ed3d35bd4d21a91edd
saksijas <mustafa@linumlabs.com>
2022-06-18 21:06:57 +0200

    Adding uniformed code from 1inch

---
+++ tests-old/src/generic.js
@@ -0,0 +1,47 @@
+import fs from "fs";
+
+const sim_options_nanos = {
+  model: "nanos",
+  logging: true,
+  start_delay: 2000,
+  X11: true,
+  custom: "",
+};
+
+const sim_options_nanox = {
+  model: "nanox",
+  logging: true,
+  start_delay: 2000,
+  X11: true,
+  custom: "",
+};
+
+const Resolve = require("path").resolve;
+const NANOS_ETH_ELF_PATH = Resolve("elfs/ethereum_nanos.elf");
+const NANOX_ETH_ELF_PATH = Resolve("elfs/ethereum_nanox.elf");
+const NANOS_COMPOUND_LIB = { Lido: Resolve("elfs/compound_nanos.elf") };
+const NANOX_COMPOUND_LIB = { Lido: Resolve("elfs/compound_nanox.elf") };
+
+const TIMEOUT = 1000000;
+
+const getTmpPath = (testName) => {
+  let date = new Date();
+  let tmpPath = `snapshots/tmp/${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}@${testName}/`;
+  fs.mkdir(tmpPath, { recursive:true }, (err) => {
+    if(err) {
+      console.log("couldn't create tmp folder at path: " + tmpPath)
+    }
+  });
+  return tmpPath;
+}
+
+module.exports = {
+    NANOS_ETH_ELF_PATH,
+    NANOX_ETH_ELF_PATH,
+    NANOS_COMPOUND_LIB,
+    NANOX_COMPOUND_LIB,
+    sim_options_nanos,
+    sim_options_nanox,
+    TIMEOUT,
+    getTmpPath,
+}
