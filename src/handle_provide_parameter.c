68de3636d3900da2e477e4f065456f368b1abe00
saksijas <mustafa@linumlabs.com>
2022-11-23 15:17:05 +0100

    result ok before printing offset

--- src/handle_provide_parameter.c
+++ src/handle_provide_parameter.c
@@ -113,6 +113,7 @@ void liquidate_borrow(ethPluginProvideParameter_t *msg, context_t *context) {
 void handle_provide_parameter(void *parameters) {
     ethPluginProvideParameter_t *msg = (ethPluginProvideParameter_t *) parameters;
     context_t *context = (context_t *) msg->pluginContext;
+    msg->result = ETH_PLUGIN_RESULT_OK;
 
     if ((context->offset) && msg->parameterOffset != context->checkpoint + context->offset) {
         PRINTF("offset: %d, checkpoint: %d, parameterOffset: %d\n",
@@ -121,7 +122,6 @@ void handle_provide_parameter(void *parameters) {
                msg->parameterOffset);
         return;
     }
-    msg->result = ETH_PLUGIN_RESULT_OK;
 
     // if (context->selectorIndex != CETH_MINT) {
     //     switch (msg->parameterOffset) {
