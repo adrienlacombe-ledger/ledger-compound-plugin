99e2cdbff4806d6fd535129145a8733c634286b8
saksijas <mustafa@linumlabs.com>
2022-11-24 16:09:59 +0100

    renaming type of counter

--- src/handle_init_contract.c
+++ src/handle_init_contract.c
@@ -2,44 +2,34 @@
 
 // Called once to init.
 void handle_init_contract(void *parameters) {
-    // Cast the msg to the type of structure we expect (here, ethPluginInitContract_t).
     ethPluginInitContract_t *msg = (ethPluginInitContract_t *) parameters;
 
-    // Make sure we are running a compatible version.
     if (msg->interfaceVersion != ETH_PLUGIN_INTERFACE_VERSION_LATEST) {
-        // If not the case, return the `UNAVAILABLE` status.
         msg->result = ETH_PLUGIN_RESULT_UNAVAILABLE;
         return;
     }
 
-    // Double check that the `context_t` struct is not bigger than the maximum size (defined by
-    // `msg->pluginContextLength`).
     if (msg->pluginContextLength < sizeof(context_t)) {
+        PRINTF("Plugin parameters structure is bigger than allowed size\n");
         msg->result = ETH_PLUGIN_RESULT_ERROR;
         return;
     }
 
     context_t *context = (context_t *) msg->pluginContext;
-
-    // Initialize the context (to 0).
     memset(context, 0, sizeof(*context));
 
-    // Look for the index of the selectorIndex passed in by `msg`.
-    size_t i;
-    PRINTF("Selector entering loop");
+    uint8_t i;
     for (i = 0; i < NUM_SELECTORS; i++) {
         if (memcmp((uint8_t *) PIC(COMPOUND_SELECTORS[i]), msg->selector, SELECTOR_SIZE) == 0) {
             context->selectorIndex = i;
-            PRINTF("Selector: %d\n", i);
-            PRINTF("Selector Index: %d\n", context->selectorIndex);
             break;
         }
     }
 
     // If `i == NUM_SELECTORS` it means we haven't found the selector. Return an error.
-    // if (i == NUM_SELECTORS) {
-    //     msg->result = ETH_PLUGIN_RESULT_UNAVAILABLE;
-    // }
+    if (i == NUM_SELECTORS) {
+        msg->result = ETH_PLUGIN_RESULT_UNAVAILABLE;
+    }
 
     // Set `next_param` to be the first field we expect to parse.
 
