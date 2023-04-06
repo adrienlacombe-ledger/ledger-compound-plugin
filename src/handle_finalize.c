0b532b189719d39f2e0ddde480b11460393d910c
saksijas <mustafa@linumlabs.com>
2023-04-06 02:57:44 +0200

    fix: adding extra screens

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -13,31 +13,31 @@ void handle_finalize(void *parameters) {
     // Setting number of screens based on function
     switch (context->selectorIndex) {
         case COMPOUND_MINT:
-            msg->numScreens = 2;
+            msg->numScreens += 1;
             break;
         case COMPOUND_REDEEM:
-            msg->numScreens = 2;
+            msg->numScreens += 1;
             break;
         case COMPOUND_REDEEM_UNDERLYING:
-            msg->numScreens = 2;
+            msg->numScreens += 1;
             break;
         case COMPOUND_BORROW:
-            msg->numScreens = 2;
+            msg->numScreens += 1;
             break;
         case COMPOUND_REPAY_BORROW:
-            msg->numScreens = 2;
+            msg->numScreens += 1;
             break;
         case COMPOUND_REPAY_BORROW_ON_BEHALF:
-            msg->numScreens = 1;
+            msg->numScreens += 0;
             break;
         case COMPOUND_TRANSFER:
-            msg->numScreens = 1;
+            msg->numScreens += 0;
             break;
         case COMPOUND_LIQUIDATE_BORROW:
-            msg->numScreens = 2;
+            msg->numScreens += 2;
             break;
         case CETH_MINT:
-            msg->numScreens = 2;
+            msg->numScreens += 2;
             break;
         // Keep this
         default:
